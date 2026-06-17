a220c0b7f3802c002a6dc6deaca888d75fa802a8
Vishnupriyan <vishnupriyan466@gmail.com>
2026-06-17 12:51:45 +0530

    Admin frontend base structure

---
+++ frontend/src/pages/admin/TurfApprovals.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function TurfApprovals() {
+  return <div>Turf Approvals Page</div>
+}
---
+++ frontend/src/services/adminApi.js
@@ -0,0 +1,5 @@
+import axiosInstance from './axiosInstance'
+
+export const adminApi = {
+  // API methods will go here
+}
